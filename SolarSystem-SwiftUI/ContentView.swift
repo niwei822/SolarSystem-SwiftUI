96bc3f9c789ff33343d60e30961e045a66811921
Niwei Li <cecily.li.822@gmail.com>
2022-07-06 13:45:17 -0700

    Initial Commit

---
+++ SolarSystem-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SolarSystem-SwiftUI
+//
+//  Created by new on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SolarSystem-SwiftUI/SolarSystem_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystem_SwiftUIApp.swift
+//  SolarSystem-SwiftUI
+//
+//  Created by new on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystem_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
